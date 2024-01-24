90cc3cc85c66c1223783d0d0466dc2f28986eccd
Kunal Singh <kunalsin9h@gmail.com>
2024-01-24 16:11:26 +0530

    added pre footer

---
+++ src/constants/locations.js
@@ -0,0 +1,22 @@
+export const locations = [
+	{
+		location: "indore",
+		address: "703, 7th Floor, Fortune Ambience, Near Surya Hotel South Tukoganj Indore, India",
+		number: "0731-4962965",
+	},
+	{
+		location: "jaipur",
+		address: "MK Plaza, 2nd Floor, Plot No. 8, Amrapali Marg, Nand Vihar, Vaishali Nagar, Jaipur, Rajasthan, 302021",
+		number: "0141-4063575",
+	},
+	{
+		location: "united states",
+		address: "Belmont Ave Plainview, NY 11803, New Your, United States",
+		number: "+1-213-985-3055",
+	},
+	{
+		location: "africa",
+		address: "204, Pacis Plaza, Simba Kimironko Building, Kimironko, Gasabo, Kigali, Rwanda",
+		number: "+250-780519884",
+	},
+]
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,7 @@ export default {
 				clientsBackground: "#222222e6",
 				brandGreen: "#3fa205",
 				dimWhite: "#eeeeef",
+				dimGray: "#6c757d",
 			},
 			backgroundImage: {
 				"brandBg": 'url("/bg.jpg")',
