36a6e8e9b16708ec00e26442fcf4221ec6f84d04
Kunal Singh <kunalsin9h@gmail.com>
2024-01-28 16:54:35 +0530

    added footer socials icons

---
+++ src/constants/socialIcons.js
@@ -0,0 +1,37 @@
+export const socialIcons = [
+	{
+		"icon": "facebook",
+		"link": "https://www.facebook.com/mmfinfotech",
+		"color": "#0ea5e9"
+	},
+	{
+		"icon": "twitter",
+		"link": "https://twitter.com/mmfinfotech/",
+		"color": "#06b6d4"
+	},
+	{
+		"icon": "linkedin",
+		"link": "https://in.linkedin.com/company/mmf-infotech-technologies-private-limited",
+		"color": "#3b82f6"
+	},
+	{
+		"icon": "pinterest",
+		"link": "https://in.pinterest.com/mmfinfotech/pins/",
+		"color": "#e11d48"
+	},
+	{
+		"icon": "slideshare",
+		"link": "https://www.slideshare.net/mmfinfotech3",
+		"color": "#f59e0b"
+	},
+	{
+		"icon": "mail",
+		"link": "mailto:info@mmfinfotech.com",
+		"color": "#ca8a04"
+	},
+	{
+		"icon": "instagram",
+		"link": "https://www.instagram.com/mmfinfotech/",
+		"color": "#f87171"
+	},
+]
